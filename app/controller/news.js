0990e4b59144dec45af93e2cdcb520559d9f4984
何凯 <F160980@efivestar.com>
2018-05-15 18:53:51 +0800

    初始项目

---
+++ app/controller/news.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class NewsController extends Controller {
+  async index() {
+    this.ctx.body = 'hi, news';
+  }
+  async list() {
+    // const dataList = {
+    //   list: [
+    //     { id: 1, title: 'this is news 1', url: '/news/1' },
+    //     { id: 2, title: 'this is news 2', url: '/news/2' },
+    //   ],
+    // };
+    const ctx = this.ctx;
+    const page = ctx.query.page || 1;
+    const newsList = await ctx.service.news.list(page);
+    await ctx.render('news/list.html', { list: newsList });
+    // await this.ctx.render('news/list.html', dataList);
+  }
+}
+
+module.exports = NewsController;
---
+++ config/config.default.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1526370826450_4434';
+
+  // add your config here
+  config.middleware = [];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.html': 'nunjucks',
+    },
+  };
+
+  config.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
+
+  return config;
+};
