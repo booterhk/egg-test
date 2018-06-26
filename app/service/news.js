7e15230e7e42edc4cf9817cb6889951adf051400
何凯 <F160980@efivestar.com>
2018-06-26 16:13:03 +0800

    增加 mysql插件

--- app/service/news.js
+++ app/service/news.js
@@ -5,28 +5,28 @@ const Service = require('egg').Service;
 class NewsService extends Service {
   async list(page = 1) {
     // read config
-    console.log('this.config.news', this.config.news);
-    const { serverUrl, pageSize } = this.config.news;
+    const { serverUrl } = this.config.baseUrl;
 
-    console.log('serverUrl, pageSize', serverUrl, pageSize);
+    console.log('baseUrl', serverUrl);
     // use build-in http client to GET hacker-news api
-    const { data: idList } = await this.ctx.curl(`${serverUrl}/topstories.json`, {
-      data: {
-        orderBy: '"$key"',
-        startAt: `"${pageSize * (page - 1)}"`,
-        endAt: `"${pageSize * page - 1}"`,
-      },
+    const {data} = await this.ctx.curl(`${serverUrl}/api/getMenuList`, {
+      // data: {
+      //   orderBy: '"$key"',
+      //   startAt: `"${pageSize * (page - 1)}"`,
+      //   endAt: `"${pageSize * page - 1}"`,
+      // },
       dataType: 'json',
     });
-
+    console.log('baseUrl data', data);
+    return data;
     // parallel GET detail
-    const newsList = await Promise.all(
-      Object.keys(idList).map(key => {
-        const url = `${serverUrl}/item/${idList[key]}.json`;
-        return this.ctx.curl(url, { dataType: 'json' });
-      })
-    );
-    return newsList.map(res => res.data);
+    // const newsList = await Promise.all(
+    //   Object.keys(idList).map(key => {
+    //     const url = `${serverUrl}/item/${idList[key]}.json`;
+    //     return this.ctx.curl(url, { dataType: 'json' });
+    //   })
+    // );
+    // return newsList.map(res => res.data);
   }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -16,10 +16,29 @@ module.exports = appInfo => {
     },
   };
 
-  config.news = {
-    pageSize: 5,
-    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  config.baseUrl = {
+    // pageSize: 5,
+    // http://localhost:8083/api/getMenuList
+    serverUrl: 'http://localhost:8083',
   };
-
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '123456',
+      // 数据库名
+      database: 'little_test',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
   return config;
 };
